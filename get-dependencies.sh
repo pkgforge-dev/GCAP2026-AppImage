58c9917a0daff6a23767f21953ee6f276f371b47
Link4Electronics <link4electronics@gmail.com>
2026-04-09 03:58:20 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -21,5 +21,5 @@ bsdtar -xvf GCAP2026v1.0.zip --strip-components=1
 rm -f *.zip
 
 mkdir -p ./AppDir/bin
-sed -i 's|\./jre/bin/java -jar irpf.jar|exec java -jar "$APPDIR/bin/irpf.jar" "$@"|' exec.sh
+sed -i 's|java -Xmx2048M -jar GCAP.jar|java -Xmx2048M -jar "$APPDIR/bin/GCAP.jar" "$@"|g' exec.sh
 mv -v exec.sh lib GCAP.jar GCAP.acb offline.png online.png pgd-updater.jar ./AppDir/bin
